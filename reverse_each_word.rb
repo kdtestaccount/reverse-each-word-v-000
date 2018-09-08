32cfca6c1991c4a70fd52022767441a8eb75937a
kvdoherty@gmail.com <kvdoherty@gmail.com>
2018-09-08 12:28:09 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,9 +2,13 @@ def reverse_each_word(str_of_words)
   str_to_array = str_of_words.split(" ")
   str_to_array.collect do |word|
     word.reverse 
-    puts str_to_array
   end
 end
 
 reverse_each_word("string of words")
 
+def burger(toppings)
+  toppings.collect do |topping|
+    "I love #{topping} on my burgers"
+  end
+end
