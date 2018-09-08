b23c743dbc175c8480e25a068bf79f5b9455c29d
kvdoherty@gmail.com <kvdoherty@gmail.com>
2018-09-08 12:29:51 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,11 @@
 def reverse_each_word(str_of_words)
   str_to_array = str_of_words.split(" ")
-  str_to_array.collect do |word|
+  str_to_array.collecta(&:reverse!)
     word.reverse 
   end
   puts str_to_array
 end
 
 reverse_each_word("string of words")
+
+
