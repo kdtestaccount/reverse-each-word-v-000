0e36089b648e56730c85870e49019c60fa61bed5
kvdoherty@gmail.com <kvdoherty@gmail.com>
2018-09-08 12:31:47 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,5 @@
 def reverse_each_word(str_of_words)
-  # str_to_array = str_of_words.split(" ")
-  str_of_words.collect(&:reverse!)
+  str_to_array = str_of_words.split(" ")
+  str_to_array.collect(&:reverse!)
 end
 
